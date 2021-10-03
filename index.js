6889dfaa9b963fdca6fa45561f1a43346cc1c6dc
Freelancer Ahsan <81156921+Ahsan5313@users.noreply.github.com>
2021-10-03 22:07:06 +0600

    | access, modify, delete and conversion

---
+++ index.js
@@ -0,0 +1,14 @@
+  const data = require("./friend_data.json")
+
+// console.log(data.student[1].country[0].second)
+
+
+// const data1 = {
+
+//     name : "ahsanul huq",
+//     age : 21
+// }
+
+// console.log(JSON.stringify(data1))
+
+console.log(JSON.parse('{"name" : "shibu" , "age" : 21}'))
